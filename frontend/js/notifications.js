ed71d637433e386bd3fef40a9a7885db949dd956
aryansaini9756754-max <aryansaini9756754@gamil.com>
2026-08-13 15:59:06 +0530

    Initial commit for Smart Emergency System Frontend

--- frontend/js/notifications.js
+++ frontend/js/notifications.js
@@ -8,7 +8,7 @@ const NotificationSystem = {
     
     sendAlert(title, message) {
         if ("Notification" in window && Notification.permission === "granted") {
-            new Notification(title, { body: message, icon: "images/emergency.png" });
+            new Notification(title, { body: message, icon: "images/emergency.svg" });
         } else {
             alert(`${title}\n${message}`);
         }
